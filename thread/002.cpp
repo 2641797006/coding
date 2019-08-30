ecf8bdc3f90803166c6ddd20294e59bbfae18e44
cpplay <2641797006@qq.com>
2019-08-30 16:54:52 +0800

    -

--- thread/002.cpp
+++ thread/002.cpp
@@ -3,25 +3,24 @@
 #include <thread>
 using namespace std;
 
-void sl()
+void sl(int i)
 {
-	system("sl");
+	while(1)
+		printf("线程%2d, char=\'%c\'\n", i, getchar());
 }
 
 int main()
 {
 	int i;
-
-	thread *p = new thread(sl), t;
+	thread t;
 	list<thread> lt;
 
-	lt.push_back( move(*p) );
+	for (i=0; i<100; ++i) {
+		new(&t) thread(sl, i);
+		lt.push_back( move(t) );
+	}
 
-	delete p;
-
-	cout<< "delete end" <<endl;
-
-	lt.back().join();
-
+	for (auto &x : lt)
+		x.join();
 }
 
